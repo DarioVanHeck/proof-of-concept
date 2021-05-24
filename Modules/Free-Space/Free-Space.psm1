ab49cb2edc8d1f9b44c3ba2807bd98a36aa6f3d9
DarioVanHeck <36508379+DarioVanHeck@users.noreply.github.com>
2021-05-24 16:34:08 +0200

    Delete Modules directory

--- Modules/Free-Space/Free-Space.psm1
+++
@@ -1,8 +0,0 @@
-function Space {
-    $drivespace = gwmi win32_logicaldisk | where DeviceID -eq "C:" | Select FreeSpace
-    $getfullproperty = $drivespace | Select -ExpandProperty "FreeSpace"
-    $getnumber = ($getfullproperty/1GB)
-    $freespace = [math]::Round($getnumber, 2)
-    
-    return $freespace
-    }
